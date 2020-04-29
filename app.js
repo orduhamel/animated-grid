614a4e9c5d3a5fa0a1e496a7031098e09a98aee2
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-04-29 11:54:35 +0200

    Design the homepage

---
+++ app.js
@@ -0,0 +1,43 @@
+var image1 = document.querySelector('.imageAnim1');
+new simpleParallax(image1, {
+  scale: 1.4,
+  delay: 0.5,
+  orientation: 'left'
+});
+
+var image2 = document.querySelector('.imageAnim2');
+new simpleParallax(image2, {
+  scale: 1.8,
+  delay: 0.5,
+  orientation: 'down'
+});
+
+var image3 = document.querySelector('.imageAnim3');
+new simpleParallax(image3, {
+  scale: 1.4,
+  delay: 0.5,
+  orientation: 'up'
+});
+
+var image4 = document.querySelector('.imageAnim4');
+new simpleParallax(image4, {
+  scale: 1.6,
+  delay: 0.5,
+  orientation: 'left'
+});
+
+var image5 = document.querySelector('.imageAnim5');
+new simpleParallax(image5, {
+  scale: 1.4,
+  delay: 0.5,
+  orientation: 'left'
+});
+
+var image6 = document.querySelector('.imageAnim6');
+new simpleParallax(image6, {
+  scale: 1.8,
+  delay: 0.5,
+  orientation: 'right'
+});
+
+
